0e774329d28eaad074518d461c5a97f3b0046811
aerox <aerox.javascript@hotmail.com>
2017-06-15 12:20:05 +0200

    chore: upgrade routing library

--- index.ios.js
+++ index.ios.js
@@ -4,9 +4,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'remote-redux-devtools';
 
-import reducers from  './app/reducers/reducers';
-import api      from './app/lib/api.js';
-import AppContainer from './app/containers/AppContainer'
+import reducers from './app/reducers/reducers';
+import api from './app/lib/api.js';
+// import AppContainer from './app/containers/AppContainer'
 import Login from './app/containers/Login';
 import { SpotifySymbol, TMDBSymbol } from './app/actions/actions';
 import config from './config';
@@ -14,21 +14,24 @@ import config from './config';
 
 import {
   AppRegistry,
-  StyleSheet
+  StyleSheet,
+  View, Text
 } from 'react-native';
+
+
 
 const composeEnhancers = composeWithDevTools({ realtime: true });
 const store = createStore(reducers, composeEnhancers(
   applyMiddleware(
     // api(SpotifySymbol,  'https://private-bd796b-spotamovie.apiary-mock.com'),
-    api(SpotifySymbol,  'https://spotamovie.herokuapp.com'),
-    api(TMDBSymbol,     'https://api.themoviedb.org/3', '?api_key='+config.api_key)
+    api(SpotifySymbol, 'https://spotamovie.herokuapp.com'),
+    api(TMDBSymbol, 'https://api.themoviedb.org/3', '?api_key=' + config.api_key)
   )
 ));
 
 const App = () => (
   <Provider store={store}>
-    <AppContainer />
+    <View><Text>aa</Text></View>
   </Provider>
 )
 
