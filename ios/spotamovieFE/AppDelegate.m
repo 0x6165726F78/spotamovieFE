1a37935740a8043a89d2e4433e6a13a240e6e3a2
aerox <aerox.javascript@hotmail.com>
2017-06-15 12:19:17 +0200

    chore: add a missing native dependency

--- ios/spotamovieFE/AppDelegate.m
+++ ios/spotamovieFE/AppDelegate.m
@@ -11,18 +11,18 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-/*#import <React/RCTLinkingManager.h>    TODO: FIX THIS DEPENDENCY */
+#import <React/RCTLinkingManager.h>
 /* #import <RCTLinkingManager.h> */
 
 @implementation AppDelegate
-/*
+
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
   sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
 {
   return [RCTLinkingManager application:application openURL:url
                       sourceApplication:sourceApplication annotation:annotation];
 }
-*/
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
