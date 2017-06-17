dae3171cc0002b7c6ec9d59eb715fc97ecb6b761
SH-Stark <salmane.hajouji@me.com>
2017-06-17 11:19:47 +0200

    feat: load config file

--- index.ios.js
+++ index.ios.js
@@ -7,14 +7,15 @@ import api from './app/lib/api.js';
 import { SpotifySymbol, TMDBSymbol } from './app/actions';
 import config from './config';
 import { AppRegistry } from 'react-native';
-import createLogger from 'redux-logger'
-import Router from './app/navigation'
+import createLogger from 'redux-logger';
+import Router from './app/navigation';
+import './app/utils/languages';
 
 const composeEnhancers = composeWithDevTools({ realtime: true });
 const store = createStore(reducers, composeEnhancers(
   applyMiddleware(
-    // api(SpotifySymbol, 'https://private-bd796b-spotamovie.apiary-mock.com'),
-    api(SpotifySymbol, 'http://localhost:3000'),
+    api(SpotifySymbol, 'https://private-bd796b-spotamovie.apiary-mock.com'),
+    // api(SpotifySymbol, 'http://localhost:3000'),
     api(TMDBSymbol, 'https://api.themoviedb.org/3', '?api_key=' + config.api_key),
     createLogger()
   )
