8a5b5b54137b25f586c3b4e1ed557d931c84b36a
aerox <aerox.javascript@hotmail.com>
2017-06-18 16:53:15 +0200

    refactor: remove unused code

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,12 +1,18 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
 import movies from './moviesReducer'
 import user from './userReducer'
 import moviesSurvey from './moviesSurveyReducer'
 import moviesLiked from './moviesLikedReducer'
 import moviesDisliked from './moviesDislikedReducer'
 import movieRecomm from './movieRecommReducer'
-import moviesSkipped from './moviesSkippedReducer'
+import moviesCached from './moviesCachedReducer'
 
 export default combineReducers({
-  movies, user, moviesSurvey, moviesLiked, moviesDisliked, movieRecomm, moviesSkipped
+  movies,
+  user,
+  moviesSurvey,
+  moviesLiked,
+  moviesDisliked,
+  movieRecomm,
+  moviesCached,
 })
