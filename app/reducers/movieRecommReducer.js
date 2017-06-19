8517a5d0144d1c7eabafb8631c7e035ac11117b1
aerox <aerox.javascript@hotmail.com>
2017-06-19 23:44:02 +0200

    refactor: update movieRecommReducer

--- app/reducers/movieRecommReducer.js
+++ app/reducers/movieRecommReducer.js
@@ -4,15 +4,10 @@ const {
   GET_MOVIE_RECOMMENDATION_FAILURE,
 } = types
 
-export default (state = { movieId: undefined }, action) => {
+export default (state = [], action) => {
   switch (action.type) {
     case GET_MOVIE_RECOMMENDATION_SUCCESS:
-      return { movieId: action.response.movieId }
-
-    case GET_MOVIE_RECOMMENDATION_FAILURE:
-      console.log('ERROR IN REDUCERS:', action.error)
-      return state
-
+      return action.response.movies
     default:
       return state
   }
